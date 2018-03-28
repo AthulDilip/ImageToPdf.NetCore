4fc537bc3c19b5317c601d78932e9eccb0bfe625
Athul Dilip <athuldilip@outlook.com>
2018-03-28 13:17:03 +0530

    Updated Demo

--- ImageToPdf.NetCore.Demo/Controllers/ValuesController.cs
+++ ImageToPdf.NetCore.Demo/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace ImageToPdf.NetCore.Demo.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            byte[] pdfBytes = ImageToPdfConvertor.Convert(@"C:\Users\athul\source\repos\ImageToPdf.NetCore\ImageToPdf.NetCore.Demo\Images\sample1.jpg");
+            byte[] pdfBytes = ImageToPdfConvertor.Convert(@"C:\Users\athul\source\repos\ImageToPdf.NetCore\ImageToPdf.NetCore.Demo\Images\sample.jpg");
             return File(pdfBytes, "application/pdf");
         }
     }
